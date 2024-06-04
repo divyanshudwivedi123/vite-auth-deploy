ec68596165ddd0b073fef8d42e0710b48e82c323
dd <dwivedidivyanshu30@gmail.com>
2024-06-05 00:48:14 +0530

    added workflow

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        secure: false,
-      },
-    },
-  },
   plugins: [react()],
+  base: "/vite-auth-deploy/", // YOUR REPO NAME HERE
 });
